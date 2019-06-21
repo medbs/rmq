5a83819e4a6a5e640a555b40be59d06700bdf342
medbs <medbs@medbs>
2019-06-21 10:12:12 +0100

    refactoring consumer

--- rmq-consumer/main.go
+++ rmq-consumer/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	"flag"
 	"encoding/json"
+	"flag"
 	"fmt"
-	"github.com/andreagrandi/go-amqp-example/contracts"
-  "github.com/streadway/amqp"
+	"github.com/streadway/amqp"
 	"log"
 )
 
+// The flag was used in the declaration because the connections start
+// with amqp.Dial(), and it requires a command line argument or environment variable.
 var (
 	amqpURI = flag.String("amqp", "amqp://guest:guest@localhost:5672/", "AMQP URI")
 )
+
+var conn *amqp.Connection
+var ch *amqp.Channel
+var replies <-chan amqp.Delivery
+var message string
 
 func failOnError(err error, msg string) {
 	if err != nil {
@@ -19,24 +25,12 @@ func failOnError(err error, msg string) {
 		panic(fmt.Sprintf("%s: %s", msg, err))
 	}
 }
-
-func init() {
-	flag.Parse()
-	initAmqp()
-}
-
-var conn *amqp.Connection
-var ch *amqp.Channel
-var replies <-chan amqp.Delivery
-
-
-
 func initAmqp() {
 	var err error
 	var q amqp.Queue
 
 	conn, err = amqp.Dial(*amqpURI)
-	failOnError(err, "Failed to connect to RabbitMQ")
+	failOnError(err, "connection to RabbitMQ failed")
 
 	log.Printf("got Connection, getting Channel...")
 
@@ -47,12 +41,12 @@ func initAmqp() {
 
 	err = ch.ExchangeDeclare(
 		"rmq-exchange", // name of the exchange
-		"direct",           // type
-		true,               // durable
-		false,              // delete when complete
-		false,              // internal
-		false,              // noWait
-		nil,                // arguments
+		"direct",       // type
+		true,           // durable
+		false,          // delete when complete
+		false,          // internal
+		false,          // noWait
+		nil,            // arguments
 	)
 	failOnError(err, "Failed to declare the Exchange")
 
@@ -60,38 +54,43 @@ func initAmqp() {
 
 	q, err = ch.QueueDeclare(
 		"rmq.queue", // name, leave empty to generate a unique name
-		true,            // durable
-		false,           // delete when usused
-		false,           // exclusive
-		false,           // noWait
-		nil,             // arguments
+		true,        // durable
+		false,       // delete when usused
+		false,       // exclusive
+		false,       // noWait
+		nil,         // arguments
 	)
 	failOnError(err, "Error declaring the Queue")
 
 	log.Printf("declared Queue (%q %d messages, %d consumers), binding to Exchange (key %q)",
-		q.Name, q.Messages, q.Consumers, "go-test-key")
+		q.Name, q.Messages, q.Consumers, "rmq-routingkey")
 
 	err = ch.QueueBind(
-		q.Name,             // name of the queue
-		"rmq.routingkey",      // bindingKey
-		"rmq-exchange", // sourceExchange
-		false,              // noWait
-		nil,                // arguments
+		q.Name,           // name of the queue
+		"rmq-routingkey", // bindingKey
+		"rmq-exchange",   // sourceExchange
+		false,            // noWait
+		nil,              // arguments
 	)
 	failOnError(err, "Error binding to the Queue")
 
 	log.Printf("Queue bound to Exchange, starting Consume (consumer tag %q)", "go-amqp-example")
 
 	replies, err = ch.Consume(
-		q.Name,            // queue
-		"go-amqp-example", // consumer
-		true,              // auto-ack
-		false,             // exclusive
-		false,             // no-local
-		false,             // no-wait
-		nil,               // args
+		q.Name,         // queue
+		"rmq-consumer", // consumer
+		true,           // auto-ack
+		false,          // exclusive
+		false,          // no-local
+		false,          // no-wait
+		nil,            // args
 	)
 	failOnError(err, "Error consuming the Queue")
+}
+
+func init() {
+	flag.Parse()
+	initAmqp()
 }
 
 func main() {
@@ -100,10 +99,8 @@ func main() {
 
 	for r := range replies {
 		log.Printf("Consuming reply number %d", count)
-		user := contracts.User{}
-		json.Unmarshal(r.Body, &user)
-		fmt.Printf("FirstName: %s, LastName: %s\n", user.FirstName, user.LastName)
+		json.Unmarshal(r.Body, &message)
+		fmt.Printf(message)
 		count++
-}
-  fmt.Println("zz")
+	}
 }
